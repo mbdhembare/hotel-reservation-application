ee0cae4d4c641d6e44b63ad4c50ba14aec4bc9f5
Monali Dhembare <monadhembare19@gmail.com>
2025-01-18 19:34:33 +0530

    added Tester class to test code

---
+++ src/Tester.java
@@ -0,0 +1,8 @@
+import model.Customer;
+
+public class Tester {
+    public static void main(String[] args) {
+        Customer customer =new Customer("monali", "dhenbare", "emain@bn");
+        System.out.println(customer);
+    }
+}
